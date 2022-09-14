067c242306ecb2fd34af113afeee3a0cc789d349
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-09-14 14:36:56 +0700

    adding validation for register

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Form, Button, Container } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'
-import RegisterImg from './RegisterImg'
+import { NavLink } from 'react-router-dom';
+import RegisterImg from './RegisterImg';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
     const [input, setInput] = useState({
@@ -10,7 +12,7 @@ const Register = () => {
         password: ""
     })
 
-    const [data,setData] = useState([]);
+    const [data] = useState([]);
 
     const getdata = (e) => {
         const { value, name } = e.target;
@@ -21,7 +23,37 @@ const Register = () => {
                 [name]: value
             }
         })
+    }
 
+    const saveData = (e) => {
+        e.preventDefault();
+
+        const { name, email, password } = input;
+
+        if (name === "") {
+                toast.error('name field is requred!',{
+                position: "top-center",
+            });
+        } else if (email === "") {
+                toast.error('email field is requred',{
+                position: "top-center",
+            });
+        } else if (!email.includes("@")) {
+                toast.error('enter a valid email address',{
+                position: "top-center",
+            });
+        } else if (password === "") {
+                toast.error('password field is requred',{
+                position: "top-center",
+            });
+        } else if (password.length < 5) {
+                toast.error('password length greater five',{
+                position: "top-center",
+            });
+        } else {
+            console.log("data added succesfully");
+            localStorage.setItem("user",JSON.stringify([...data,input]));
+        }
     }
 
     return (
@@ -44,13 +76,14 @@ const Register = () => {
                                 <Form.Label>Password</Form.Label>
                                 <Form.Control type="password" name='password' onChange={getdata} placeholder="Password" />
                             </Form.Group>
-                            <Button className="w-100" lg={6} style={{ background: "rgb(67, 185, 127)" }} type="submit">
+                            <Button className="w-100" lg={6} onClick={saveData} style={{ background: "rgb(67, 185, 127)" }} type="submit">
                                 Submit
                             </Button>
                         </Form>
-                        <p className='mt-3'>Already Have an Account <span><NavLink to="/login">Login</NavLink></span> </p>
+                        <p className='mt-3'>Already Have an Account <span><NavLink to="#">Login</NavLink></span> </p>
                     </div>
                 </section>
+                <ToastContainer />
             </Container>
         </>
     )
