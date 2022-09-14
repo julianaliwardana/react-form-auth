2886d7d7bbe4d7c3bf1e2eab03d78015c8358fe3
Julian_A_Wardana <julian.wardana@binus.ac.id>
2022-09-14 23:11:09 +0700

    final commit

---
+++ src/Components/LoginImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LoginImage = () => {
+    return (
+        <>
+            <div className="left-section w-100 d-flex justify-content-center align-content-center">
+                <div className="login-img mt-5">
+                    <img src="./login.svg" style={{ maxWidth: 400 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default LoginImage;
---
+++ src/Components/RegisterImage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const RegisterImage = () => {
+    return (
+        <>
+            <div className="left-section w-100 d-flex justify-content-center align-content-center">
+                <div className="register-img mt-5">
+                    <img src="./register.svg" style={{ maxWidth: 400 }} alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RegisterImage;
